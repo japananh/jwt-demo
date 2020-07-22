d2e68e1539e3758170f960bdeca6a23d0dea74ba
seta-nana <anhtn6797@seta-international.vn>
2020-07-23 00:02:26 +0700

    send token to client

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 
 // Import Routes
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/user", authRoute);
+app.use("/api/user", postRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 const {
   registerValidation,
@@ -11,11 +12,11 @@ router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
   const { error } = registerValidation(req.body);
-  if (error) return res.status(401).send({ error });
+  if (error) return res.status(400).send({ error: error.details[0].message });
 
   const userExist = await User.findOne({ email });
   if (userExist)
-    return res.status(400).send({ error: "This email already exists." });
+    return res.status(409).send({ error: "This email already exists." });
 
   const hashedPassword = await encryptData(password);
 
@@ -25,7 +26,7 @@ router.post("/register", async (req, res) => {
 
     res.status(200).send({ success: true, data: { id: savedUser._id } });
   } catch (error) {
-    res.status(400).send(error);
+    res.status(500).send(error);
   }
 });
 
@@ -33,18 +34,19 @@ router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   const { error } = loginValidation(req.body);
-  if (error) return res.status(401).send({ error });
+  if (error) return res.status(404).send({ error: error.details[0].message });
 
   const userExist = await User.findOne({ email });
   if (!userExist)
-    return res.status(401).send({ error: "Invalid email or password." });
+    return res.status(400).send({ error: "Invalid email or password." });
 
   const isValidPassword = await decryptData(password, userExist.password);
   if (!isValidPassword || !userExist.email)
-    return res.status(401).send({ error: "Invalid email or password." });
+    return res.status(400).send({ error: "Invalid email or password." });
 
-  const { name, email: userEmail, _id: id } = userExist;
-  return res.status(200).send({ id, name, email: userEmail });
+  const token = jwt.sign({ id: userExist._id }, process.env.SECRET_TOKEN);
+  res.header("auth-token", token);
+  res.status(200).send({ token });
 });
 
 module.exports = router;
